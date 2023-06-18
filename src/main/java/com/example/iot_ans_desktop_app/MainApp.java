45e6d3aef59d8c540c9acced6dc349476b72a8c4
Marcin Golonka <BattelShark@gmail.com>
2023-06-18 19:28:32 +0200

    Dodanie backendu - połączenia ESP autoryzacji itp

---
+++ src/main/java/com/example/iot_ans_desktop_app/MainApp.java
@@ -0,0 +1,30 @@
+package com.example.iot_ans_desktop_app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("home_screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 740);
+        stage.setTitle("Aplikacja_IoT");
+        stage.setScene(scene);
+        stage.show();
+
+        // Pobierz dane z ESP32 - Wilgotność
+        SensorDataFetcher.fetchSensorData("https://ha.salonar.pl/api/states/sensor.iot_esp_ans_wilgotnosc_1");
+
+        // Pobierz dane z ESP32 - Temperatura
+        SensorDataFetcher.fetchSensorData("https://ha.salonar.pl/api/states/sensor.iot_esp_ans_temperatura_1");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/iot_ans_desktop_app/SensorDataFetcher.java
@@ -0,0 +1,38 @@
+package com.example.iot_ans_desktop_app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SensorDataFetcher {
+    private static final String AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiZjViYWE2NWFmNWI0MTU2OWY3ZGY2ZWI4NTA4Y2Y0YyIsImlhdCI6MTY4NzEwNDg0MiwiZXhwIjoyMDAyNDY0ODQyfQ.K8hXuDLbONxjn9chtViIcDO1IKmd-P-ZHvP2YQSs8RY";
+
+    public static void fetchSensorData(String sensorUrl) {
+        try {
+            URL url = new URL(sensorUrl);
+            HttpURLConnection connection = Connection_ESP.openConnection(url, AUTH_TOKEN);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Przetwarzanie odpowiedzi
+                String responseData = response.toString();
+                DataProcessor.processSensorData(responseData);
+            } else {
+                // Obsługa błędu
+                System.out.println("Wystąpił błąd: " + responseCode);
+            }
+        } catch (IOException e) {
+            // Obsługa wyjątku
+            System.out.println("Wystąpił błąd: " + e.getMessage());
+        }
+    }
+}
